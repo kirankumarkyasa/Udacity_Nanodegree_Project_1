a9417e608d9cdddde143891dd3f834ac2ad5807f
kiran.kyasa <kiran.kyasa@xcubelabs.com>
2016-04-12 13:55:34 +0530

    1. Added Retro Manager 2. Added Model Class without Properties 3. Added MovieService Interface for Retrofit

---
+++ app/src/main/java/Model/Movie.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ * Created by kiran on 12/4/16.
+ */
+public class Movie {
+
+
+
+}
---
+++ app/src/main/java/WebServices/MovieService.java
@@ -0,0 +1,9 @@
+package WebServices;
+
+/**
+ * Created by kiran on 12/4/16.
+ */
+public interface MovieService {
+
+
+}
